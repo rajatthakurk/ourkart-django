970446ea88c85aca6434ab87652bbb257344cadf
rajatthakurk@gmail.com <rajatthakurk@gmail.com>
2021-08-27 17:46:56 +0530

    cart update

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from carts.models import CartItem,Cart
 from django.contrib import messages,auth
 from django.http import HttpResponse
 from accounts.models import Account
@@ -11,6 +12,8 @@ from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
 from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage, message
+
+from carts.views import _cart_id
 
 
 def register(request):
@@ -62,6 +65,19 @@ def login(request):
         user = auth.authenticate(email=email,password=password)
 
         if user is not None:
+            try:
+                cart = Cart.objects.get(cart_id = _cart_id(request))
+                is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+                if is_cart_item_exists:
+                    cart_item = CartItem.objects.filter(cart=cart)
+
+                    for item in cart_item:
+                        item.user = user
+                        item.save()
+
+            except:
+                pass
+
             auth.login(request,user)
             messages.success(request,'You Are Now Logged In')
             return redirect('dashboard')
