e1752b0f7b7d6830f343bb4c0d2e6474c2d13acf
rajatthakurk@gmail.com <rajatthakurk@gmail.com>
2021-08-28 14:04:27 +0530

    create orders app and models

---
+++ orders/admin.py
@@ -0,0 +1,8 @@
+from typing import Pattern
+from django.contrib import admin
+from .models import Payment,Order,OrderProduct
+
+admin.site.register(Order)
+admin.site.register(Payment)
+admin.site.register(OrderProduct)
+
